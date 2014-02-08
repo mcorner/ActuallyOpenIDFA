24d55277cb540ade17e1b7fa662536f25eb573b8
Mark Corner <mcorner@gmail.com>
2014-02-08 17:36:53 -0500

    Initial Commit

---
+++ aoi/aoi.h
@@ -0,0 +1,13 @@
+//
+//  aoi.h
+//  aoi
+//
+//  Created by Mark Corner on 2/8/14.
+//  Copyright (c) 2014 Mark Corner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface aoi : NSObject
+
+@end
---
+++ aoi/aoi.m
@@ -0,0 +1,13 @@
+//
+//  aoi.m
+//  aoi
+//
+//  Created by Mark Corner on 2/8/14.
+//  Copyright (c) 2014 Mark Corner. All rights reserved.
+//
+
+#import "aoi.h"
+
+@implementation aoi
+
+@end
